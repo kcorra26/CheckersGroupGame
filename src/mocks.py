f166b3e6d26151021b647b7e0397c9eec8ee08ef
Jaslin Aguirre <77134520+JaslinAg@users.noreply.github.com>
2023-02-20 23:54:12 -0600

    added list of list for gui testing

--- src/mocks.py
+++ src/mocks.py
@@ -1,10 +1,23 @@
 from enum import Enum
 TeamColor = Enum("TeamColor",  ["RED", "BLACK", "EMPTY"]) 
 
+B = 'black'
+R = 'red'
+
 class StubCheckerboard:
-    def __init__(self, n):
+    def __init__(self, n=3):
         self.n = n
         self.width = 2*n + 2
+        self.board=[[None, B, None, B, None,  B, None, B],
+                    [B, None, B, None, B, None, B, None ],
+                    [None, B, None, None, None,  B, None, B],
+                    [None, None, None, B, None, None, None, None],
+                    [None, None, R, None, None, None, None, None],
+                    [R, None, R, None, R, None, None, None],
+                    [None, R, None, R, None, R, None, R],
+                    [R, None, R, None, R, None, R, None]]
 
     def __str__(self):
         return("board")
+    
+

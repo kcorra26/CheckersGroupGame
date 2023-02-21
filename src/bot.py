6292267a2dd3b74e441fb6526913aa123a532df5
Kathleen Elisa Corra <kcorra@linux4.cs.uchicago.edu>
2023-02-21 17:22:17 -0600

    first versions of mock bot game and smart bot

--- src/bot.py
+++ src/bot.py
@@ -12,7 +12,8 @@ import random
 from typing import Union # idk what this does yet
 
 import click
-from checkers import Board, Empty, Piece, King, TeamColor # BoardType?
+from checkers import Board, Game, Piece, TeamColor 
+from mocks import CheckersGameBotMock
 
 
 class RandomBot: 
@@ -20,25 +21,25 @@ class RandomBot:
     Simple Bot that just picks a move at random
     """
 
-    _board: BoardType # this is just what they have
+    #_game: Game # this is just what they have
     _color: TeamColor # again
     _opponent_color: TeamColor
 
-    def __init__(self, board: BoardType, color: TeamColor, 
-                 _opponent_color: TeamColor)
+    def __init__(self, game, color: TeamColor, 
+                 opponent_color: TeamColor):
         """
         Constructor
 
         Args: 
-            board: Board the bot will play on
+            game: Game the bot will play on
             color: Bot's team color
             opponent_color: Opponent's color
         """
-        self._board = board
+        self._game = game
         self._color = color
         self._opponent_color = opponent_color
     
-    def suggest_move(self) # -> int:
+    def suggest_move(self): # -> int:
         """
         Suggests a move 
 
@@ -46,7 +47,7 @@ class RandomBot:
         """
         # why does the docstring say it returns none if the example code
         # returns random.choice(possible_cols)?
-        move_dict = board.all_team_moves(self._color)
+        move_dict = self._game.all_team_moves(self._color)
         og_pos = random.choice(list(move_dict))
         end_pos = random.choice(move_dict[og_pos])
 
@@ -65,11 +66,11 @@ class SmartBot:
     # research other strategies
     """
 
-    _board: BoardType # idk if we will use this
+    #_game: BoardType # idk if we will use this
     _color: TeamColor
     _opponent_color: TeamColor
 
-    def __init__(self, board: BoardType, color: TeamColor, 
+    def __init__(self, game, color: TeamColor, 
                 opponent_color: TeamColor):
         """
         Constructor
@@ -80,11 +81,11 @@ class SmartBot:
             opponent_color: Opponent's color
         """
 
-        self._board = board
+        self._game = game
         self._color = color
         self._opponent_color = opponent_color
 
-    def suggest_move(self) # -> int:
+    def suggest_move(self): # -> int:
         """
         Suggests a move
 
@@ -94,6 +95,7 @@ class SmartBot:
         # if there are just a bunch with one move, pick one at random
 
         """
+        https://hobbylark.com/board-games/Checkers-Strategy-Tactics-How-To-Win 
         - if you have the chance to jump more than one way, jump toward the
         center. 
             - if you have the choice between moving or jumping to the side
@@ -106,11 +108,113 @@ class SmartBot:
         - try not to separate pieces??
             - give preference to the decisions that keep pieces close 
             to eachother
+            - but avoid a triangle situation that would make it easy for the 
+            other team to jump
+        - consider opportunities for jumping (thinking ahead to see if a jump 
+        is possible, then further to see if a jump on that one is possible)
+            - maybe have a jump counter? for each move
+            - idk how to label the moves
         
+        https://www.thesprucecrafts.com/how-to-win-at-checkers-411170 
+        - prioritize getting a checker to the end of the board
         """
+        # assumming that when there is at least one opportunity to jump, 
+        # all_team_moves consists only of those jumping moves
+        move_dict = self._game.all_team_moves(self._color) 
 
+        center = self._game.width // 2
 
-class BotPlayer:
+        # if there is just one move in the dictionary (ie if there is one 
+        # key and that key just has one tuple in its list):
+        if self.one_move(move_dict) is not None:
+            # return the position of the key and the first (and only) value 
+            # in the value list
+            return self.one_move(move_dict)
+            # WORKS
+        # TODO: make a helper function to check and return if there is only
+        
+        king_moves = {} # if there are no king moves, will be empty
+        consider = {} # if there are no jumps, will just be all moves
+        max_jumps = 0
+        centermost = {}
+        dist_from_center = center
+
+        for start_pos, list_moves in move_dict.items(): #nested loop ew
+            for end_pos in list_moves:
+                row, col = end_pos
+                # if it's a winning move, do it
+                if self._game.is_winning_move(start_pos, end_pos, self._color):
+                    return (start_pos, end_pos)
+
+                # if the number of jumps is greater than the current max
+                if self._game.num_jumps(start_pos, end_pos) > max_jumps:
+                    max_jumps = self._game.num_jumps(start_pos, end_pos)
+                    consider = {start_pos : [end_pos]} # reset dict
+                # if the number of jumps is equal to the current max
+                elif self._game.num_jumps(start_pos, end_pos) == max_jumps:
+                    lst = consider.get((start_pos), [])
+                    lst.append(end_pos)
+                    consider[start_pos] = lst
+                # otherwise, stop considering it
+                else: 
+                    continue
+        if self.one_move(consider) is not None:
+            return self.one_move(consider)
+        
+        for start_pos, list_moves in consider.items():
+            for end_pos in list_moves:
+                if self._game.will_king(start_pos, end_pos): # if moving a piece to this 
+                # position would make it a king
+                     # add the move to make_king_moves 
+                    temp_lst = king_moves.get((start_pos), [])
+                    temp_lst.append(end_pos)
+                    king_moves[start_pos] = temp_lst
+        if self.one_move(king_moves) is not None:
+            return self.one_move(king_moves)
+        elif king_moves == {}:
+            consider2 = consider
+        else:
+            consider2 = king_moves
+
+        for start_pos, list_moves in consider2.items():
+            for end_pos in list_moves:
+                row, col = end_pos
+                if abs(col - (self._game.width // 2)) < dist_from_center:
+                    dist_from_center = abs(col - (self._game.width // 2))
+                    centermost = {start_pos : [end_pos]}
+                elif abs(col - (self._game.width // 2)) == dist_from_center:
+                    centermost.get(start_pos, []).append(end_pos)
+        
+        if self.one_move(centermost) is not None:
+            return self.one_move(centermost)
+        elif centermost != {}: # whether consider2 is 
+            og_pos = random.choice(list(centermost))
+            end_pos = random.choice(centermost[og_pos])
+            print("random from centermost")
+            return (og_pos, end_pos)
+        else: 
+            og_pos = random.choice(list(consider2))
+            end_pos = random.choice(consider2[og_pos])
+            print("random from either king or jumps, centermost empty")
+            return (og_pos, end_pos)
+
+        # is a dictionary a good way to implement this, considering I have to
+        # do a nested for loop?
+
+        # is it better to do multiple for loops?
+    def one_move(self, dic): # WORKS
+        """
+        Returns the value in the given dic if there is only one value, other
+        wise returns none. 
+        Learned to do this here: https://stackoverflow.com/questions/46042430/best-way-to-get-a-single-key-from-a-dictionary 
+        """
+        if len(dic) == 1 and len(dic[next(iter(dic))]) == 1:
+            return (next(iter(dic)), dic[next(iter(dic))][0])
+        return None
+
+                
+        
+class BotPlayer: # playing against each other
     """
     Simple class to store information about a both player in a simulation.
     """
@@ -120,8 +224,8 @@ class BotPlayer:
     color: TeamColor
     wins: int
 
-    def __init__(self, name: str, board: BoardType, color: TeamColor,
-                 opponent_color: TeamColor)
+    def __init__(self, name: str, game, color: TeamColor,
+                 opponent_color: TeamColor):
         """
         Constructor
 
@@ -134,13 +238,13 @@ class BotPlayer:
         self.name = name
 
         if self.name == "random":
-            self.bot = RandomBot(board, color, opponent_color)
+            self.bot = RandomBot(game, color, opponent_color)
         elif self.name == "smart":
-            self.bot = SmartBot(board, color, opponent_color)
+            self.bot = SmartBot(game, color, opponent_color)
         self.color = color
         self.wins = 0
     
-    def simulate(board: BoardType, n: int, bots) # -> None:
+    def simulate(game, n: int, bots): # -> None:
         """
         Simulate multiple games between two bots
 
@@ -152,16 +256,17 @@ class BotPlayer:
         
         Returns: None
         """
+        # CANT PLAY THIS UNTIL MOCK is_done() and 
         for _ in range(n):
             # Reset the board
             # board.reset()
 
-            # starting player
+            # starting player 
             #current = bots[TeamColor.COLOR1] idk what color goes first
 
-            while not board.is_done(): # board class needs an is_done function
+            while not game.is_done(): # game class needs an is_done function
                 og_pos, new_pos = current.bot.suggest_move() # two tuples
-                board.move_piece(og_pos, new_pos, current.color) # need to pass
+                game.move_piece(og_pos, new_pos, current.color) # need to pass
                 # team as a parameter
 
                 # update the player 
@@ -171,15 +276,15 @@ class BotPlayer:
                 elif current.color == TeamColor.COLOR2:
                     current = bots[TeamColor.COLOR1]
                 
-                winner = board.get_winner() # if we make a get_winner function
+                winner = game.get_winner() # if we make a get_winner function
                 # otherwise, do 
-                # if winner = board.is_winner(TeamColor.COLOR1):
-                # elif winner = board.is_winner(TeamColor.Color2):
+                # if winner = game.is_winner(TeamColor.COLOR1):
+                # elif winner = game.is_winner(TeamColor.Color2):
                 if winner is not None:
                     bots[winner].wins += 1
                 
 @click.command(name="checkers-bot")
-@click.option("-n", "--num-games", type=click.INT, default-10000)
+@click.option("-n", "--num-games", type=click.INT, default=10000)
 @click.option("--player1", 
               type=click.Choice(['random', 'smart'], case_sensitive=False),
               default="random")
@@ -187,14 +292,14 @@ class BotPlayer:
               type=click.Choice(['random', 'smart'], case_sensitive=False),
               default="random")
 def cmd(num_games, player1, player2):
-    board = Board()
+    game = Game()
                                     # bot1 team, opponent
-    bot1 = BotPlyaer(player1, board, TeamColor.COLOR1, TeamColor.COLOR2)
-    bot2 = BotPlyaer(player1, board, TeamColor.COLOR2, TeamColor.COLOR1)
+    bot1 = BotPlayer(player1, game, TeamColor.COLOR1, TeamColor.COLOR2)
+    bot2 = BotPlayer(player1, game, TeamColor.COLOR2, TeamColor.COLOR1)
 
     bots = {TeamColor.COLOR1: bot1, TeamColor.COLOR2: bot2}
 
-    simulate(board, num_games, bots) # simulating for a number of games
+    simulate(game, num_games, bots) # simulating for a number of games
 
     bot1_wins = bots[TeamColor.COLOR1].wins # number of wins
     bot2_wins = bots[TeamColor.COLOR2].wins 
--- src/mocks.py
+++ src/mocks.py
@@ -19,5 +19,41 @@ class StubCheckerboard:
 
     def __str__(self):
         return("board")
+
+class CheckersGameBotMock: # game mock?
+    def __init__(self, n=3):
+        self.n = n 
+        self.width = 2*n + 2
+        # self.board?
+    
+    def __str__(self) -> str:
+        return "BOARD"
+    
+    def all_team_moves(self, team):
+        return {(1, 2) : [(2, 3), (2, 1)], (3, 2) : [(5, 3), (5, 0), (7, 0)], 
+                (6, 2) : [(7, 3), (7, 0)]} 
+
+    def is_done(self):
+        return False
+    
+    def will_king(self, og_pos, end_pos): 
+        row, col = end_pos
+        if row == self.width - 1:
+            return True
+        return False
+    
+    def num_jumps(self, og_pos, end_pos):
+        og_row, _ = og_pos
+        end_row, _ = end_pos
+        return abs(end_row - og_row - 1)
+    
+    def is_winning_move(self, og_pos, end_pos, team):
+        #row, _ = og_pos
+        #if row == 6:
+            #return True
+        return False
+
+
+
     
 

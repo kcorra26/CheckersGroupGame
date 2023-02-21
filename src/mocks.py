46e3da9c4a9cc777ef950d57a6419146672eadcb
Jaslin Aguirre <77134520+JaslinAg@users.noreply.github.com>
2023-02-21 16:20:53 -0600

    more edits to mocks for gui purposes

--- src/mocks.py
+++ src/mocks.py
@@ -8,16 +8,27 @@ class StubCheckerboard:
     def __init__(self, n=3):
         self.n = n
         self.width = 2*n + 2
-        self.board=[[None, B, None, B, None,  B, None, B],
-                    [B, None, B, None, B, None, B, None ],
-                    [None, B, None, None, None,  B, None, B],
-                    [None, None, None, B, None, None, None, None],
-                    [None, None, R, None, None, None, None, None],
-                    [R, None, R, None, R, None, None, None],
-                    [None, R, None, R, None, R, None, R],
-                    [R, None, R, None, R, None, R, None]]
+        self.red_pieces = [Piece('RED', (1,0)), Piece('RED', (3,0), True)]
+        self.black_pieces = [Piece('BLACK', (4,5), True), Piece('BLACK', (7,0)), Piece('Black', (4,3))]
+
+        self.board=[[None, Piece('BLACK'), None, Piece('BLACK'), None,  Piece('BLACK'), None, Piece('BLACK')],
+                    [Piece('BLACK'), None, Piece('BLACK'), None, Piece('BLACK'), None, Piece('BLACK'), None ],
+                    [None, Piece('BLACK'), None, Piece('BLACK', True), None, Piece('BLACK'), None, Piece('BLACK')],
+                    [None, None, None, None, None, None, None, None],
+                    [None, None, None, None, None, None, None, None],
+                    [Piece('RED', True), None, Piece('RED'), None, Piece('RED'), None, Piece('RED', True), None],
+                    [None, Piece('RED'), None, Piece('RED'), None, Piece('RED'), None, Piece('RED')],
+                    [Piece('RED'), None, Piece('RED'), None, Piece('RED'), None, Piece('RED'), None]]
 
     def __str__(self):
         return("board")
-    
+
+class Piece:
+    def __init__(self, team:TeamColor, pos:tuple = (0,0), is_king=False):
+        '''
+        pos = (col, row) or (x,y) with (0,0) being in the top right
+        '''
+        self.team = team
+        self.is_king = is_king
+        self.pos = pos
 

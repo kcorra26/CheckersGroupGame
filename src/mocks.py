02df7c0a75279a6aae14e28965f13cbd8e9b5d60
Jaslin Aguirre <77134520+JaslinAg@users.noreply.github.com>
2023-02-22 16:45:03 -0600

    made changes based on position tuple

--- src/mocks.py
+++ src/mocks.py
@@ -9,28 +9,29 @@ class StubCheckerboard:
         self.n = n
         self.width = 2*n + 2
         self.red_pieces = [Piece('RED', (1,0)), Piece('RED', (3,0), True)]
-        self.black_pieces = [Piece('BLACK', (4,5), True), Piece('BLACK', (7,0)), Piece('Black', (4,3))]
+        self.black_pieces = [Piece('BLACK', (4,5), True), Piece('BLACK', (7,0)), Piece('BLACK', (2,3))]
 
-        self.board=[[None, Piece('BLACK'), None, Piece('BLACK'), None,  Piece('BLACK'), None, Piece('BLACK')],
+        '''self.board=[[None, Piece('BLACK'), None, Piece('BLACK'), None,  Piece('BLACK'), None, Piece('BLACK')],
                     [Piece('BLACK'), None, Piece('BLACK'), None, Piece('BLACK'), None, Piece('BLACK'), None ],
                     [None, Piece('BLACK'), None, Piece('BLACK', True), None, Piece('BLACK'), None, Piece('BLACK')],
                     [None, None, None, None, None, None, None, None],
                     [None, None, None, None, None, None, None, None],
                     [Piece('RED', True), None, Piece('RED'), None, Piece('RED'), None, Piece('RED', True), None],
                     [None, Piece('RED'), None, Piece('RED'), None, Piece('RED'), None, Piece('RED')],
-                    [Piece('RED'), None, Piece('RED'), None, Piece('RED'), None, Piece('RED'), None]]
+                    [Piece('RED'), None, Piece('RED'), None, Piece('RED'), None, Piece('RED'), None]]'''
 
     def __str__(self):
         return("board")
     
     def move_piece(self, old_pos, new_pos:tuple = (0,0)):
-        col, row = old_pos
-        piece = self.get_piece(col, row)
+        row, col = old_pos
+        piece = self.get_piece(row, col)
         piece.update_pos(new_pos)
     
-    def get_piece(self, col, row):
+    def get_piece(self, row, col):
         for piece in self.red_pieces + self.black_pieces:
-            if piece.pos == (col,row):
+            if piece.pos == (row, col):
+                print('found piece')
                 return piece
         return None
     
@@ -40,13 +41,17 @@ class StubCheckerboard:
 class Piece:
     def __init__(self, team:TeamColor, pos:tuple = (0,0), is_king=False):
         '''
-        pos = (col, row) or (x,y) with (0,0) being in the top right
+        pos = (row, col) or (y,x) with (0,0) being in the top right
         '''
         self.team = team
         self.is_king = is_king
+        self.col = pos[1]
+        self.row = pos[0]
         self.pos = pos
     
     def update_pos(self, new_pos):
+        self.col = new_pos[1]
+        self.row = new_pos[0]
         self.pos = new_pos
 
 class CheckersGameBotMock: # game mock?

a0c226be49a50cda2f0dc05856d07b8ed493eae2
Jaslin Aguirre <77134520+JaslinAg@users.noreply.github.com>
2023-02-22 16:44:47 -0600

    deleted sprite for poss_moves

--- src/sprites.py
+++ src/sprites.py
@@ -41,25 +41,7 @@ class PieceSprite(pygame.sprite.Sprite):
     def update(self):
         '''
         called in every frame - updates the position of the sprite
+        piece.pos = (row, col) = (y, x)
         '''
-        self.rect.x = self.sq_size * self.piece.pos[0]
-        self.rect.y = self.sq_size * self.piece.pos[1]
-
-class PossibleMovesSprite(pygame.sprite.Sprite):
-    '''
-    This class represents the moves that are possible for a certain sprite
-    '''
-    def __init__(self, x_pos, y_pos, window):
-        '''
-        constructor for a PossibleMovesSprite
-
-        Args:
-            pos(tuple): a touple representing the row, col 
-            sq_size(int): the size of the board squares 
-        '''
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface([20, 20])
-        self.image.fill(WHITE)
-        self.image.set_colorkey(WHITE)
-        pygame.draw.circle(window, YELLOW, (x_pos, y_pos), 20)
-        self.rect = self.image.get_rect()
+        self.rect.x = self.sq_size * self.piece.col
+        self.rect.y = self.sq_size * self.piece.row
